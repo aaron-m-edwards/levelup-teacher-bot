fcc7a468c152608f7b9d34c87c1e684ef9421805
Aaron Edwards <aedwards@thoughtworks.com>
2017-11-30 10:52:06 +1100

    Oauth Stuff

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Botkit = require('botkit');
 const oauthRegistration = require('./oauthRegistration');
-const debug = console.log;
 
 const controller = Botkit.slackbot();
 
@@ -9,7 +8,7 @@ controller.configureSlackApp({
   clientSecret: process.env.clientSecret,
   rtm_receive_messages: false,
   redirectUri: 'https://levelup-slack-teacher.herokuapp.com/oauth',
-  scopes: ['bot'],
+  scopes: ['bot']
 });
 
 controller.setupWebserver(process.env.PORT,function(err,webserver) {
@@ -18,38 +17,22 @@ controller.setupWebserver(process.env.PORT,function(err,webserver) {
       if (err) {
         res.status(500).send('ERROR: ' + err);
       } else {
-        var code = req.query.code;
-        var state = req.query.state;
-        var slackapi = controller.spawn({});
-        var opts = {
-          client_id: controller.config.clientId,
-          client_secret: controller.config.clientSecret,
-          code: code
-        };
-        slackapi.api.oauth.access(opts, function(err, auth) {
-          if (err) {
-            debug('Error confirming oauth', err);
-            return res.send("Login Error");
-          }
-          var scopes = auth.scope.split(/\,/);
-          slackapi.api.auth.test({token: auth.access_token}, function(err, identity) {
-            if (err) {
-              debug('Error fetching user identity', err);
-              return res.send("Login Error");
-            }
-            auth.identity = identity;
-            controller.trigger('oauth:success', [auth]);
-            res.cookie('team_id', auth.team_id);
-            res.cookie('bot_user_id', auth.bot.bot_user_id);
-            return res.send("Woot!");
-          });
-        });
+        res.send('Success!');
       }
     })
     .createWebhookEndpoints(controller.webserver);
   controller.startTicking();
 });
-oauthRegistration(controller);
+
+oauthRegistration.registerSuccess(controller);
+
+controller.storage.teams.all((err, teams) => {
+  for (var t in teams) {
+    if (teams[t].bot) {
+      controller.spawn(teams[t])
+    }
+  }
+});
 
 controller.hears(['hi', 'hello'], 'direct_message', function (bot, message) {
   bot.api.users.info({user: message.user}, (error, response) => {
