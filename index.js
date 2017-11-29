b46076f9baf9036a0edc3edef5b3bd573f4ecb06
Aaron Edwards <aedwards@thoughtworks.com>
2017-11-30 09:35:21 +1100

    spawn a bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Botkit = require('botkit');
-
+const oauthRegistration = require('./oauthRegistration');
+const debug = console.log;
 
 const controller = Botkit.slackbot();
 
@@ -8,7 +9,7 @@ controller.configureSlackApp({
   clientSecret: process.env.clientSecret,
   rtm_receive_messages: false,
   redirectUri: 'https://levelup-slack-teacher.herokuapp.com/oauth',
-  scopes: ['incoming-webhook','team:read','users:read','channels:read','im:read','im:write','groups:read','emoji:read','chat:write:bot']
+  scopes: ['bot'],
 });
 
 controller.setupWebserver(process.env.PORT,function(err,webserver) {
@@ -17,13 +18,38 @@ controller.setupWebserver(process.env.PORT,function(err,webserver) {
       if (err) {
         res.status(500).send('ERROR: ' + err);
       } else {
-        res.send('Success!');
+        var code = req.query.code;
+        var state = req.query.state;
+        var slackapi = controller.spawn({});
+        var opts = {
+          client_id: controller.config.clientId,
+          client_secret: controller.config.clientSecret,
+          code: code
+        };
+        slackapi.api.oauth.access(opts, function(err, auth) {
+          if (err) {
+            debug('Error confirming oauth', err);
+            return res.send("Login Error");
+          }
+          var scopes = auth.scope.split(/\,/);
+          slackapi.api.auth.test({token: auth.access_token}, function(err, identity) {
+            if (err) {
+              debug('Error fetching user identity', err);
+              return res.send("Login Error");
+            }
+            auth.identity = identity;
+            controller.trigger('oauth:success', [auth]);
+            res.cookie('team_id', auth.team_id);
+            res.cookie('bot_user_id', auth.bot.bot_user_id);
+            return res.send("Woot!");
+          });
+        });
       }
     })
     .createWebhookEndpoints(controller.webserver);
   controller.startTicking();
 });
-
+oauthRegistration(controller);
 
 controller.hears(['hi', 'hello'], 'direct_message', function (bot, message) {
   bot.api.users.info({user: message.user}, (error, response) => {
