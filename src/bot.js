246b74475a786220e0d385ab3074471ec985b84c
Aaron Edwards <aedwards@thoughtworks.com>
2017-11-30 14:25:21 +1100

    Fixed the call me command

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,12 @@
 module.exports = function(controller) {
   controller.hears(/call me (.*)/i, 'direct_message', function(bot, message){
-    const newName = message.match[1];
-    controller.storage.users.save({id: message.user, newName }, console.log);
-    bot.reply(message, `Ok I will call you ${newName} from now on`);
-  })
+    const name = message.match[1];
+    controller.storage.users.save({id: message.user, name }, console.log);
+    bot.reply(message, `Ok I will call you ${name} from now on`);
+  });
+
   controller.hears(['hi', 'hello'], 'direct_message', function (bot, message) {
     controller.storage.users.get(message.user, function(err, user){
-      controller.storage.teams.all(function(err, all_team_data){console.log(all_team_data)});
       if(user && user.name) {
         bot.reply(message, `Hi there ${user.name}`)
       } else {
