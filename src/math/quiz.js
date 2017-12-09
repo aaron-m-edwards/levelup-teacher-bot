224e338a4fe0268eb1adf880b78072d402e4ab32
aaron-m-edwards <aedwards@thoughtworks.com>
2017-12-09 14:43:02 +1100

    Added a you got x correct message

--- src/math/quiz.js
+++ src/math/quiz.js
@@ -14,11 +14,15 @@ function askQuestion(convo, number, quiz) {
   convo.addQuestion(`Question ${number+1}: What is ${question.number1} + ${question.number2}?`, (response, convo) => {
     if (response.text === `${question.answer}`) {
       convo.say('That is correct, good job!');
+      question.correctAnswer = true;
     } else {
       convo.say(`That is incorrect, the correct answer is ${question.answer}`);
+      question.correctAnswer = false;
     }
     if(quiz[number+1]) {
       askQuestion(convo, number+1, quiz)
+    } else {
+      convo.say(`You got ${quiz.filter(q => q.correctAnswer).length}/${quiz.length} correct`)
     }
     convo.next();
   })
--- src/math/quiz.test.js
+++ src/math/quiz.test.js
@@ -21,6 +21,10 @@ describe("math quiz", () => {
           number1: 1,
           number2: 2,
           answer: 3
+        }, {
+          number1: 2,
+          number2: 2,
+          answer: 4
         }]
       }
     }
@@ -53,6 +57,16 @@ describe("math quiz", () => {
       .then(() => testBot.sendMessage(bot, ''))
       .then(() => testBot.sendMessage(bot, '4'))
       .then(message => expect(message.text).to.contain('the correct answer is 3'));
+  })
+
+  it('should show the amout of correct answers', () => {
+    return testBot.sendMessage(bot, 'start')
+      .then(() => testBot.sendMessage(bot, ''))
+      .then(() => testBot.sendMessage(bot, '4'))
+      .then(() => testBot.sendMessage(bot, ''))
+      .then(() => testBot.sendMessage(bot, '4'))
+      .then(() => testBot.sendMessage(bot, ''))
+      .then(message => expect(message.text).to.contain('You got 1/2 correct'));
   })
 
 });
